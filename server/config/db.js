49e55003127303356b09ebf6c393cac85c6f32ff
nischay-chauhan <masternischay@outlook.com>
2023-12-30 14:48:08 +0530

    Backend pt:1

--- server/config/db.js
+++ server/config/db.js
@@ -1,15 +1,17 @@
 import mongoose from "mongoose";
 
-const dbConnection = async () => {
+const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI, {
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
-            useUnifiedTopology: true,
+             useUnifiedTopology: true,
+            
         });
-        console.log("Database connected");
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
-        console.log(error);
+        console.error(`Error: ${error.message}`);
+        process.exit(1);
     }
 }
 
-export default dbConnection
+export default connectDB
--- server/server.js
+++ server/server.js
@@ -1,21 +1,27 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import cookieParser from 'cookie-parser'
-import cors from 'cors'
-import dbConnection from './config/db.js';
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+import connectDB from './config/db.js';
+import cookieParser from 'cookie-parser';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import userRoutes from './routes/userRoutes.js';
+
+const port = process.env.PORT || 5000;
+
+connectDB();
 
 const app = express();
-dotenv.config();
-
-dbConnection();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use(cookieParser());
-app.use(cors());
 
-const PORT = process.env.PORT || 5000;
+app.use('/api/users', userRoutes);
 
-app.listen(PORT , (err) => {
-    if(err) throw err;
-    console.log(`Server running on port ${PORT}`);
-})
+
+
+app.use(notFound);
+app.use(errorHandler);
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
