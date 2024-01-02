b80bf0cf8a74800934f4b71ab8266e35258fe4da
nischay-chauhan <masternischay@outlook.com>
2024-01-02 22:22:06 +0530

    little responsive

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 const Home = () => {
   return (
     <div className="w-full h-screen flex justify-center items-center" style={{ backgroundImage: `url(/home.avif)`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
-      <div className="relative z-10">
-        <h1 className="md:text-8xl sm:8xl  font-bold bg-clip-text text-transparent" style={{ backgroundImage: 'linear-gradient(to right, #4F46E5, #D74DF6)' }}>
+      <div className="relative z-10 text-center">
+        <h1 className="text-5xl sm:text-6xl md:text-8xl lg:text-8xl font-bold bg-clip-text text-transparent" style={{ backgroundImage: 'linear-gradient(to right, #4F46E5, #D74DF6)' }}>
           Welcome to Skribble
         </h1>
       </div>
