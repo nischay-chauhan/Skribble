7723f72ba5b39cbb9cc568ce3db5a4a58e6d34f8
nischay-chauhan <masternischay@outlook.com>
2024-01-04 14:47:54 +0530

    About

--- client/src/components/About/About.jsx
+++ client/src/components/About/About.jsx
@@ -1,7 +1,18 @@
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div className="mt-16 max-w-md mb-16  mx-auto">
+      <h1 className="text-3xl font-bold mb-4">About Skribble Clone</h1>
+      <p className="text-gray-700">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed accumsan velit vel libero tincidunt, ac consequat risus finibus. Nunc non elit in eros lacinia fringilla. Aliquam quis efficitur neque. In hac habitasse platea dictumst. Integer ut urna auctor, egestas ligula sit amet, malesuada felis. Suspendisse potenti. Curabitur eget purus augue. Vestibulum eu ligula tincidunt, auctor nisi vel, tincidunt nisl. Proin vel nunc nec quam varius dapibus ut at sem.
 
-export default About
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed accumsan velit vel libero tincidunt, ac consequat risus finibus. Nunc non elit in eros lacinia fringilla. Aliquam quis efficitur neque. In hac habitasse platea dictumst. Integer ut urna auctor, egestas ligula sit amet, malesuada felis. Suspendisse potenti. Curabitur eget purus augue. Vestibulum eu ligula tincidunt, auctor nisi vel, tincidunt nisl. Proin vel nunc nec quam varius dapibus ut at sem.
+
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed accumsan velit vel libero tincidunt, ac consequat risus finibus. Nunc non elit in eros lacinia fringilla. Aliquam quis efficitur neque. In hac habitasse platea dictumst. Integer ut urna auctor, egestas ligula sit amet, malesuada felis. Suspendisse potenti. Curabitur eget purus augue. Vestibulum eu ligula tincidunt, auctor nisi vel, tincidunt nisl. Proin vel nunc nec quam varius dapibus ut at sem.
+
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed accumsan velit vel libero tincidunt, ac consequat risus finibus. Nunc non elit in eros lacinia fringilla. Aliquam quis efficitur neque. In hac habitasse platea dictumst. Integer ut urna auctor, egestas ligula sit amet, malesuada felis. Suspendisse potenti. Curabitur eget purus augue. Vestibulum eu ligula tincidunt, auctor nisi vel, tincidunt nisl. Proin vel nunc nec quam varius dapibus ut at sem.
+      </p>
+    </div>
+  );
+};
+
+export default About;
