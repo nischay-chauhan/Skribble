78ee17a0f0a9f401507924d018b5df45c1f90531
nischay-chauhan <masternischay@outlook.com>
2024-01-04 14:31:07 +0530

    home

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 const Home = () => {
   return (
     <div className="w-full h-screen flex justify-center items-center" style={{ backgroundImage: `url(/home.avif)`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
